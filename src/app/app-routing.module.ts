7c8dc5f27df08f1c9c1b62ef9c030e1fe2dcdf70
hoseaBBPR <hosea@breadandbutterpr.com>
2019-10-29 09:09:25 -0400

    preview test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { HomeComponent } from './home/home.component';
 
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent}
+  { path: '', component: HomeComponent},
+  { path: '/:slug', component: HomeComponent}
 
 
 ];
